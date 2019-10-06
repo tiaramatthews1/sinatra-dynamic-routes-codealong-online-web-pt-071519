a280aaba0314796e9c872cb5687e63badef16bfc
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-06 16:17:26 +0000

    Automatically backed up by Learn

--- spec/sinatradynamic_routes_codealong_spec.rb
+++ spec/sinatradynamic_routes_codealong_spec.rb
@@ -2,34 +2,34 @@ require 'spec_helper'
 
 describe 'app.rb' do
   describe 'GET /goodbye/:name' do
-    xit 'sends a 200 status code' do
+    it 'sends a 200 status code' do
       get '/goodbye/danny'
       expect(last_response.status).to eq(200)
     end
 
-    xit "displays 'goodbye' and a name" do
+    it "displays 'goodbye' and a name" do
       get '/goodbye/danny'
       expect(last_response.body).to include("Goodbye, danny.")
     end
 
-    xit "is not hard-coded" do
+    it "is not hard-coded" do
       get '/goodbye/fannie'
       expect(last_response.body).to include("Goodbye, fannie.")
     end
   end
 
   describe 'GET /multiply/:num1/:num2' do
-    xit 'sends a 200 status code' do
+    it 'sends a 200 status code' do
       get '/multiply/5/5'
       expect(last_response.status).to eq(200)
     end
 
-    xit 'displays the product of the two numbers in the route' do
+    it 'displays the product of the two numbers in the route' do
       get '/multiply/6/6'
       expect(last_response.body).to include("36")
     end
 
-    xit 'is not hard-coded' do
+    it 'is not hard-coded' do
       get '/multiply/7/7'
       expect(last_response.body).to include("49")
       expect(last_response.body).to_not include("36")
